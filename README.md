7ced0796e52670a06088195f4d86e63d816c9820
Dheep net-research <dheep@net-research.com.au>
2018-10-25 22:46:24 +1100

    device list and networks

--- README.md
+++ README.md
@@ -6,3 +6,10 @@ This repository is used for Operating Meraki devices using Python-Django framewo
 
 - 0.01
     -- Simple python command line tool to pull Meraki device status
+
+# How to get API Key from meraki
+
+For access to the API, first enable the API for your organization under Organization > Settings > Dashboard API access.  
+
+API key: Dashboard > upper-right email login > My profile > API access
+Organization ID: https://api.meraki.com/api/v0/organizations in same browser session
--- merakiops_app.py
+++ merakiops_app.py
@@ -35,6 +35,20 @@ def get_api_creds(apikeycsvfile):
 
     return api_access[0]
 
+def get_api_call(apiurl, urlheaders):
+    """ Make an API call and return as dictionary
+
+    Syntax: get_api_call('api url', 'headers')
+    output: return_code, dictionary of JSON object returned.
+
+    """
+
+    print (apiurl, urlheaders)
+    response = requests.get(apiurl, headers=urlheaders)
+    json_output = json.loads(response.text)
+    return response, json_output
+    # return '200', {'id':'1', 'name':'test'}
+
 
 instance_apikey = get_api_creds('accessapi.key')
 
@@ -42,14 +56,21 @@ print(instance_apikey)
 
 api_key = instance_apikey['api_key']
 org_id = str(instance_apikey['org_id'])
-org_url = 'https://dashboard.meraki.com/api/v0/organizations'
+
+meraki_api_url = 'https://dashboard.meraki.com/api/v0/'
 meraki_headers = {'x-cisco-meraki-api-key': api_key, 'content-type': 'application/json'}
+
+org_url = meraki_api_url + 'organizations'
+
 # get all of the organizations this api key has access to
-response = requests.get(org_url, headers=meraki_headers)
-json_output = json.loads(response.text)
+# response = requests.get(org_url, headers=meraki_headers)
+# json_output = json.loads(response.text)
+
 # loop through the json_output and print each row
-for row in json_output:
-    print(row)
+rcode, json_dict = get_api_call(org_url, meraki_headers)
+for row in json_dict:
+    print(row, type(row))
+    print(rcode, type(rcode))
     print("Org ID: " + str(row['id']))
     print("Org Name: " + row['name'])
 
@@ -57,8 +78,27 @@ print(type(org_id))
 
 network_url = 'https://api.meraki.com/api/v0/organizations/' + org_id + '/networks'
 print(network_url)
-response = requests.get(network_url, headers=meraki_headers)
-json_output = json.loads(response.text)
 
-for row in json_output:
+rcode, json_dict = get_api_call(network_url, meraki_headers)
+print(json_dict)
+for row in json_dict:
     print(row)
+    print(row['id'])
+    vlan_network_url = 'https://api.meraki.com/api/v0/networks/' + row['id'] + '/devices'
+    print(vlan_network_url)
+    
+    vlanrcode, vlan_json_dict = get_api_call(vlan_network_url, meraki_headers)
+    print("\n\n")
+    print(vlan_json_dict)
+    for item in vlan_json_dict:
+        dev_url = 'https://api.meraki.com/api/v0/devices/' + item['serial'] + '/clients?timespan=7200'
+        #/devices/[serial]/clients
+
+        clientrcode, client_json_dict = get_api_call(dev_url, meraki_headers)
+        
+        print("\n\n")
+        # print(client_json_output)
+        for i in client_json_dict:
+            print(i)
+            print(i['dhcpHostname'], i['description'], i['ip'])
+
